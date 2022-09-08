381472b8907fcb7ea38ba2ed2f1db67448ec55f6
Abhishek <abhibhardwaj1017@gmail.com>
2022-09-08 19:00:15 +0530

    Feat : Infinite Scroll Added in Games page

--- src/components/games/Games.jsx
+++ src/components/games/Games.jsx
@@ -5,13 +5,27 @@ import { Link } from "react-router-dom";
 import { FaFirefoxBrowser } from "react-icons/fa";
 import { IoGameController } from "react-icons/io5";
 import { AiFillWindows } from "react-icons/ai";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Games = ({ fetchData }) => {
+  const [allGames, setAllGames] = useState([]);
   const [gameData, setGameData] = useState([]);
+
   useEffect(() => {
     window.scrollTo(0, 0);
-    fetchData().then((data) => setGameData(data.slice(0, 75)));
+    const fetchGames = async () => {
+      const data = await fetchData();
+      setAllGames(data)
+      setGameData(allGames.slice(0, 15));
+    }
+    fetchGames();
   }, []);
+
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      setGameData(allGames.slice(0, gameData.length + 15));
+    }, 500);
+  }
 
   if (gameData.length === 0) {
     return (
@@ -28,55 +42,68 @@ const Games = ({ fetchData }) => {
       </h2>
 
       <Container>
-        <Row className="justify-content-around">
-          {gameData.map((game) => {
-            const { title, short_description, genre, platform, thumbnail, id } =
-              game;
-            return (
-              <Col xl="3" md="4" className="game-card p-0 my-4 mx-3" key={id}>
-                <Link to={`/games/id/${id}`} className="game-card">
-                  <div className="game-card-img">
-                    <Image fluid src={thumbnail} alt={title} />
-                  </div>
-                  <div className="game-card-body text-secondary  px-3">
-                    <div className=" d-flex justify-content-between mt-2">
-                      <p className="fs-5 fw-bold mb-1">{title}</p>
-                      <p className="fs-5 mb-1">
-                        <Badge bg="success">Free</Badge>
-                      </p>
+        <InfiniteScroll
+          dataLength={gameData.length}
+          next={fetchMoreData}
+          hasMore={gameData.length !== allGames.length}
+          loader={
+            allGames.length !== gameData.length && (
+              <SpinneR message="Fetching More Games" fs="fs-2" />
+            )
+          }
+          style={{ overflow: "hidden" }}
+        >
+          <Row className="justify-content-around">
+            {gameData.map((game) => {
+              const { title, short_description, genre, platform, thumbnail, id } =
+                game;
+              return (
+                <Col xl="3" md="4" className="game-card p-0 my-4 mx-3" key={id}>
+                  <Link to={`/games/id/${id}`} className="game-card">
+                    <div className="game-card-img">
+                      <Image fluid src={thumbnail} alt={title} />
                     </div>
+                    <div className="game-card-body text-secondary  px-3">
+                      <div className=" d-flex justify-content-between mt-2">
+                        <p className="fs-5 fw-bold mb-1">{title}</p>
+                        <p className="fs-5 mb-1">
+                          <Badge bg="success">Free</Badge>
+                        </p>
+                      </div>
 
-                    <div className="fs-6" style={{ textAlign: "justify" }}>
-                      <p className="mb-0">
-                        {short_description.slice(0, 120)}...
-                      </p>
-                    </div>
+                      <div className="fs-6" style={{ textAlign: "justify" }}>
+                        <p className="mb-0">
+                          {short_description.slice(0, 120)}...
+                        </p>
+                      </div>
 
-                    <div className=" d-flex justify-content-start py-2">
-                      <p className="fs-5 mb-1 me-4">
-                        <Badge bg="secondary">{genre}</Badge>
-                      </p>
-                      <p className="fs-5 mb-1 me-4">
-                        <Badge bg="danger">
-                          <IoGameController />
-                        </Badge>
-                      </p>
-                      <p className="fs-5 mb-1">
-                        <Badge bg="primary">
-                          {platform === "PC (Windows)" ? (
-                            <AiFillWindows />
-                          ) : (
-                            <FaFirefoxBrowser />
-                          )}
-                        </Badge>
-                      </p>
+                      <div className=" d-flex justify-content-start py-2">
+                        <p className="fs-5 mb-1 me-4">
+                          <Badge bg="secondary">{genre}</Badge>
+                        </p>
+                        <p className="fs-5 mb-1 me-4">
+                          <Badge bg="danger">
+                            <IoGameController />
+                          </Badge>
+                        </p>
+                        <p className="fs-5 mb-1">
+                          <Badge bg="primary">
+                            {platform === "PC (Windows)" ? (
+                              <AiFillWindows />
+                            ) : (
+                              <FaFirefoxBrowser />
+                            )}
+                          </Badge>
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                </Link>
-              </Col>
-            );
-          })}
-        </Row>
+                  </Link>
+                </Col>
+              );
+            })}
+
+          </Row>
+        </InfiniteScroll>
       </Container>
     </Container>
   );
