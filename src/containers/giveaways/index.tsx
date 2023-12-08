f3e8bccf3b0a2bf1fdb228e7376961453cb7e097
Abhishek <abhibhardwaj1017@gmail.com>
2023-12-09 01:28:19 +0530

    Feat : Giveaway Added

--- src/containers/giveaways/index.tsx
+++ src/containers/giveaways/index.tsx
@@ -1,6 +1,6 @@
 import { GiveawayCard, GamesCardSkeleton, HeadContent } from "../../components";
-import { useGetGiveawaysQuery } from "../../services"
-import { motion } from "framer-motion"
+import { useGetGiveawaysQuery } from "../../services";
+import { motion } from "framer-motion";
 import { Giveaway } from "../../utils";
 
 const stagger = 0.3;
@@ -21,36 +21,32 @@ export const Giveaways = () => {
             <section className="text-gray-400 body-font py-10 shadow-inset-1 min-h-[80dvh]">
                 <div className="container px-5 py-24 mx-auto ">
                     <div className="flex flex-wrap gap-y-5 justify-center sm:justify-start">
-                        {
-                            isLoading ? (
-                                [1, 2, 3, 4, 5, 6].map((item, index) => (
-                                    <div key={item} className="w-full md:w-1/3 sm:w-1/2 p-4">
-                                        <GamesCardSkeleton key={item} />
-                                    </div>
-                                ))
-                            ) : (
-                                giveaways?.map((giveaway: Giveaway, index: number) => (
-                                    <motion.div
-                                        key={giveaway?.id}
-                                        variants={variants}
-                                        initial="hidden"
-                                        animate="visible"
-                                        transition={{
-                                            delay: index * stagger,
-                                            ease: "easeIn",
-                                            duration: 0.5,
-                                        }}
-                                        viewport={{ amount: 0 }}
-                                        className="md:w-1/3 sm:w-1/2 p-4"
-                                    >
-                                        <GiveawayCard giveaway={giveaway} />
-                                    </motion.div>
-                                ))
-                            )
-                        }
+                        {isLoading
+                            ? [1, 2, 3, 4, 5, 6].map((item, index) => (
+                                <div key={item} className="w-full md:w-1/3 sm:w-1/2 p-4">
+                                    <GamesCardSkeleton key={item} />
+                                </div>
+                            ))
+                            : giveaways?.map((giveaway: Giveaway, index: number) => (
+                                <motion.div
+                                    key={giveaway?.id}
+                                    variants={variants}
+                                    initial="hidden"
+                                    animate="visible"
+                                    transition={{
+                                        delay: index * stagger,
+                                        ease: "easeIn",
+                                        duration: 0.5,
+                                    }}
+                                    viewport={{ amount: 0 }}
+                                    className="md:w-1/3 sm:w-1/2 p-4"
+                                >
+                                    <GiveawayCard giveaway={giveaway} />
+                                </motion.div>
+                            ))}
                     </div>
                 </div>
-            </section >
+            </section>
         </>
-    )
-}
+    );
+};
