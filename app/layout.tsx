0c83d700bcc927b38ebfe64a8c5809e21be2d7c7
Abhishek <abhibhardwaj1017@gmail.com>
2024-05-23 01:50:00 +0530

    Fix : Async Server Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,7 @@ export default function RootLayout({
 			>
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen">
+						{/* @ts-ignore @ts-expect-error Async Server Component */}
 						<Navbar />
 						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
 							{children}
