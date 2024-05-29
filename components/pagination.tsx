6c485fa25bb592ed04e90993a2cc25c01124a704
Abhishek <abhibhardwaj1017@gmail.com>
2024-05-30 00:28:13 +0530

    Feat : Mobile Pagination

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -3,6 +3,8 @@
 import { Pagination as NextUIPagination } from "@nextui-org/pagination";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import { useCallback } from "react";
+import { Button } from "@nextui-org/button";
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
 interface PaginationProps {
     total: number;
@@ -42,13 +44,44 @@ export function Pagination(props: PaginationProps) {
         [createQueryString, pathname, router]
     );
 
+    function MobPagination() {
+        return (
+            <div className="flex gap-2 justify-center items-center sm:hidden">
+                <Button
+                    color="default"
+                    isIconOnly
+                    isDisabled={props.initialPage === 1}
+                    onClick={() => handleRedirect(props.initialPage - 1)}
+                    className="font-bold text-xl dark"
+                >
+                    <FaArrowCircleLeft />
+                </Button>
+                <p className="text-gray-400">
+                    Page {props.initialPage} of {props.total}
+                </p>
+                <Button
+                    color="default"
+                    isIconOnly
+                    isDisabled={props.initialPage === props.total}
+                    onClick={() => handleRedirect(props.initialPage + 1)}
+                    className="font-bold text-xl"
+                >
+                    <FaArrowCircleRight />
+                </Button>
+            </div>
+        );
+    }
+
     return (
-        <NextUIPagination
-            {...props}
-            onChange={(page: number) => handleRedirect(page)}
-            classNames={{
-                wrapper: "m-auto",
-            }}
-        />
+        <>
+            <NextUIPagination
+                {...props}
+                onChange={(page: number) => handleRedirect(page)}
+                classNames={{
+                    wrapper: "m-auto hidden sm:flex",
+                }}
+            />
+            <MobPagination />
+        </>
     );
 }
