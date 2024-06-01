4777669c399e9aa8d866244c277f61230d519dc9
Abhishek <abhibhardwaj1017@gmail.com>
2024-06-01 18:31:55 +0530

    Feat : Filters reset button

--- components/filters/advanced-filters.tsx
+++ components/filters/advanced-filters.tsx
@@ -1,18 +1,41 @@
 "use client";
 
-import { Button } from "@nextui-org/button";
+import { Button, ButtonGroup } from "@nextui-org/button";
 import { FaFilter } from "react-icons/fa";
+import { FcClearFilters } from "react-icons/fc";
+import { useRouter, usePathname } from "next/navigation";
 
 export function AdvancedFilters() {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const hangleClearAll = () => {
+        router.push(pathname);
+    };
+
     return (
-        <Button
-            size="lg"
-            radius="sm"
-            color="warning"
-            variant="flat"
-            endContent={<FaFilter />}
-        >
-            Advanced Filters
-        </Button>
+        <ButtonGroup className="w-full">
+            <Button
+                size="lg"
+                radius="sm"
+                color="warning"
+                variant="flat"
+                endContent={<FaFilter />}
+                className="w-full"
+            >
+                More
+            </Button>
+            <Button
+                size="lg"
+                radius="sm"
+                color="danger"
+                variant="flat"
+                endContent={<FcClearFilters color="inherit" />}
+                className="w-full"
+                onPress={hangleClearAll}
+            >
+                Clear All
+            </Button>
+        </ButtonGroup>
     );
 }
