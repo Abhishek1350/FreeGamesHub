6d853a5bd9df95b4778340ceeadb1bae76b22622
Abhishek <abhibhardwaj1017@gmail.com>
2024-06-02 20:07:05 +0530

    Feat : Modal for filters

--- components/filters/advanced-filters.tsx
+++ components/filters/advanced-filters.tsx
@@ -4,38 +4,65 @@ import { Button, ButtonGroup } from "@nextui-org/button";
 import { FaFilter } from "react-icons/fa";
 import { FcClearFilters } from "react-icons/fc";
 import { useRouter, usePathname } from "next/navigation";
+import {
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    useDisclosure,
+} from "@nextui-org/modal";
 
 export function AdvancedFilters() {
     const router = useRouter();
     const pathname = usePathname();
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     const hangleClearAll = () => {
         router.push(pathname);
     };
 
     return (
-        <ButtonGroup className="w-full">
-            <Button
-                size="lg"
-                radius="sm"
-                color="warning"
-                variant="flat"
-                endContent={<FaFilter />}
-                className="w-full"
+        <>
+            <ButtonGroup className="w-full">
+                <Button
+                    size="lg"
+                    radius="sm"
+                    color="warning"
+                    variant="flat"
+                    endContent={<FaFilter />}
+                    className="w-full"
+                    onPress={onOpen}
+                >
+                    More
+                </Button>
+                <Button
+                    size="lg"
+                    radius="sm"
+                    color="danger"
+                    variant="flat"
+                    endContent={<FcClearFilters color="inherit" />}
+                    className="w-full"
+                    onPress={hangleClearAll}
+                >
+                    Clear All
+                </Button>
+            </ButtonGroup>
+
+            <Modal
+                isOpen={isOpen}
+                onClose={onClose}
+                placement="center"
+                backdrop="blur"
+                className="pb-2"
+                isDismissable={false}
+                isKeyboardDismissDisabled={false}
             >
-                More
-            </Button>
-            <Button
-                size="lg"
-                radius="sm"
-                color="danger"
-                variant="flat"
-                endContent={<FcClearFilters color="inherit" />}
-                className="w-full"
-                onPress={hangleClearAll}
-            >
-                Clear All
-            </Button>
-        </ButtonGroup>
+                <ModalContent>
+                    <ModalHeader>More Filters</ModalHeader>
+                    <ModalBody>Modal</ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
     );
 }
