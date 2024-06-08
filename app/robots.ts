70bd46bb70a9155faa8ff12f6d74752cbd5a16e4
Abhishek <abhibhardwaj1017@gmail.com>
2024-06-09 01:13:46 +0530

    Change: Sitemap location in robot.txt

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       disallow: '/studio/',
     },
-    sitemap: `${currentSiteUrl}/api/sitemap`,
+    sitemap: `${currentSiteUrl}/sitemap.xml`,
   }
 }
