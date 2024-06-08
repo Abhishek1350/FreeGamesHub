9db79ff68347eb1b9e809ab2648e332b775f00b8
Abhishek <abhibhardwaj1017@gmail.com>
2024-06-09 00:58:46 +0530

    Feat : Robots txt

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+import { currentSiteUrl } from '@/lib/env'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      disallow: '/studio/',
+    },
+    sitemap: `${currentSiteUrl}/api/sitemap`,
+  }
+}
